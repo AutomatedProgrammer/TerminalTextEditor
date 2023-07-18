72784a947e56887bc29413a0720ceb732bbb9fc2
The Refrigerator <21050516+The-Frige@users.noreply.github.com>
2023-07-17 17:09:57 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,43 @@
 #include <ncurses.h>
 #include <list>
+#include <algorithm>
 #include <fstream>
 
 using namespace std;
 
 #define CTRL_KEY(k) ((k) & 0x1f) //Control key
 
+list<int>::iterator my_find(list<int>::iterator first, list<int>::iterator last, const int& val, int& x_pos)
+{
+    while (first != last) 
+    {
+        if (*first == val) return first;
+        ++first;
+        ++x_pos;
+    }
+    return last;
+}
+
+list<int>::reverse_iterator my_rfind(list<int>::reverse_iterator first, list<int>::reverse_iterator last, const int& val, int& x_pos)
+{
+    while (first != last) 
+    {
+        if (*first == val) return first;
+        else if (*first == 10)
+        ++first;
+        //Ok have a variable that marks when it hits the /n. Have it keep iterating to the /r. Subtract to get the x pos and then have it return the /n
+    }
+    return last;
+}
+
 int main(int argc, char** argv)
 {
-    int x = 0, y = 0;
+    int x = 0, y = 0, max_y = 0;
     int x_offset = 0;
-    int lc = 0;
     list<int> buffer;
     list<int>::iterator it = buffer.begin(); //Viewer iterator
     list<int>::iterator e_it = buffer.end(); //Editor iterator
+    list<int>::reverse_iterator re_it = buffer.rbegin(); //Reverse iterator for the sake of being able to move the cursor up
     int ch;
     char cch;
     ifstream ifs;
@@ -37,13 +61,18 @@ int main(int argc, char** argv)
         ch = *it;
         printw("%c", ch);
         x++;
+        if (ch == 10)
+        {
+            y++;
+            max_y++;
+            x = 0;
+        }
+        
     }
     //getsyx(y, x);
     while(true)
     {
-        //getsyx(y,x); 
         ch = getch();
-        //if (ch == 17) break;
         if (ch == CTRL_KEY('q'))
         {
             clear();
@@ -51,12 +80,23 @@ int main(int argc, char** argv)
             ch = getch();
             if (ch == CTRL_KEY('q')) break;
             else {
+                x = 0;
+                y = 0;
                 clear();
                 for (it = buffer.begin(); it != buffer.end(); it++)
                 {
                     ch = *it;
                     printw("%c", ch);
-                } 
+                    x++;
+                    if (ch == 10)
+                    {
+                        y++;
+                        max_y++;
+                        x = 0;
+                    }
+                }
+                x -= x_offset;
+                move(y,x);
                 continue;
             }
         }
@@ -77,28 +117,29 @@ int main(int argc, char** argv)
         }
         else if (ch == 263)
         {
-            //swap(*e_it, buffer.back());
-            
-            //if (e_it != buffer.end()) e_it = buffer.erase(e_it);
             if (e_it != buffer.begin() && e_it != buffer.end()) 
             {
                 buffer.erase(e_it--);
-                //x_offset++;
             }
             else if (e_it == buffer.end() && e_it != buffer.begin()) 
             {
                 buffer.pop_back();
-                //x_offset++;
             }
-            
 
             x = 0;
+            y = 0;
             clear();
             for (it = buffer.begin(); it != buffer.end(); it++)
             {
                 ch = *it;
                 printw("%c", ch);
                 x++;
+                if (ch == 10)
+                {
+                    y++;
+                    max_y++;
+                    x = 0;
+                }
             }
             x -= x_offset;
             move(y,x);
@@ -107,23 +148,58 @@ int main(int argc, char** argv)
         }
         else if (ch == KEY_UP)
         {
-            //getsyx(y, x);
-            //y--;
-            //move(y, x);
+            if (y > 0)
+            {
+                //x = 0;
+                //re_it = my_rfind(re_it, buffer.rend(), 10, x);
+                re_it = find(re_it, buffer.rend(), 10);
+                re_it++;
+                e_it = re_it.base();
+                /*
+                if (*e_it != 10)
+                {
+                    continue;
+                }
+                */
+                
+                while (*e_it != 10 && e_it != buffer.end())
+                {
+                    e_it++;
+                    x++;
+                }
+                y--;
+                move(y, x);
+            }
             continue;
         }
         else if (ch == KEY_DOWN)
         {
-            //getsyx(y, x);
-            //y++;
-            //move(y, x);
+            if (y < max_y)
+            {
+                x = 0;
+                
+                e_it = my_find(e_it, buffer.end(), 10, x);
+                e_it++;
+                /*
+                if (*e_it != 10)
+                {
+                    continue;
+                }
+                */
+                while (*e_it != 10 && e_it != buffer.end())
+                {
+                    e_it++;
+                    x++;
+                }
+                y++;
+                move(y, x);
+            }
             continue;
         }
         else if (ch == KEY_RIGHT)
         {
             if (e_it != buffer.end())
             {
-                //getsyx(y, x);
                 x_offset--;
                 x++;
                 e_it++;
@@ -133,9 +209,8 @@ int main(int argc, char** argv)
         }
         else if (ch == KEY_LEFT)
         {
-            if (e_it != buffer.begin() && x-1 > 0)
+            if (e_it != buffer.begin() && x - 1 > 0)
             {
-                //getsyx(y, x);
                 x_offset++;
                 x--;
                 e_it--;
@@ -147,6 +222,12 @@ int main(int argc, char** argv)
         
         buffer.insert(e_it, ch);
         x++;
+        if (ch == 10)
+        {
+            y++;
+            max_y++;
+            x = 0;
+        }
         for (it = buffer.begin(); it != buffer.end(); it++)
         {
             ch = *it;
@@ -163,7 +244,4 @@ int main(int argc, char** argv)
     return 0;
 }
 //TODO
-//Let the user move the cursor around the lines and insert and delete.
-//1. We got the insertion between lines.
-//2. Now we just need to figure out how to delete between lines.
-//3. Add in the second dimension and this project is practically done!
+//1. Add in the second dimension.
